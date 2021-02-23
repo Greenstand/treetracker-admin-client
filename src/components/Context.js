b1322e19df5f2d1ca25f0e8845d3539e5c1cbf15
Gwynn Dandridge-Perry <gwynndp@gmail.com>
2021-02-23 15:06:15 -0800

    fix: console errors on login (#19)
    * fix: console error for displaying roles
    
    * fix: console errors on login
    
    * fix: update package
    
    Co-authored-by: Nick Charlton <nmcharlton@hotmail.com>

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import isEqual from 'react-fast-compare'
+import React from 'react';
+import isEqual from 'react-fast-compare';
 
-import TreeImageScrubber from './TreeImageScrubber'
-import Planters from './Planters'
-import Trees from './Trees'
-import Account from './Account'
-import Home from './Home'
-import Users from './Users'
-import SpeciesMgt from './SpeciesMgt'
+import TreeImageScrubber from './TreeImageScrubber';
+import Planters from './Planters';
+import Trees from './Trees';
+import Account from './Account';
+import Home from './Home';
+import Users from './Users';
+import SpeciesMgt from './SpeciesMgt';
 
-import IconSettings from '@material-ui/icons/Settings'
-import IconShowChart from '@material-ui/icons/ShowChart'
-import IconThumbsUpDown from '@material-ui/icons/ThumbsUpDown'
-import IconNature from '@material-ui/icons/Nature'
-import IconGroup from '@material-ui/icons/Group'
-import IconCompareArrows from '@material-ui/icons/CompareArrows'
-import IconPermIdentity from '@material-ui/icons/PermIdentity'
-import CategoryIcon from '@material-ui/icons/Category'
-import HomeIcon from '@material-ui/icons/Home'
+import IconSettings from '@material-ui/icons/Settings';
+import IconShowChart from '@material-ui/icons/ShowChart';
+import IconThumbsUpDown from '@material-ui/icons/ThumbsUpDown';
+import IconNature from '@material-ui/icons/Nature';
+import IconGroup from '@material-ui/icons/Group';
+import IconCompareArrows from '@material-ui/icons/CompareArrows';
+import IconPermIdentity from '@material-ui/icons/PermIdentity';
+import CategoryIcon from '@material-ui/icons/Category';
+import HomeIcon from '@material-ui/icons/Home';
 
-import { session, hasPermission, POLICIES } from '../models/auth'
-import axios from 'axios'
-import Unauthorized from './Unauthorized'
+import { session, hasPermission, POLICIES } from '../models/auth';
+import axios from 'axios';
+import Unauthorized from './Unauthorized';
 
-export const AppContext = React.createContext({})
+export const AppContext = React.createContext({});
 
 function getRoutes(user) {
   return [
@@ -46,34 +46,31 @@ function getRoutes(user) {
       linkTo: '/verify',
       component: TreeImageScrubber,
       icon: IconThumbsUpDown,
-      disabled: !hasPermission(
-        user, [
-          POLICIES.SUPER_PERMISSION,
-          POLICIES.LIST_TREE,
-          POLICIES.APPROVE_TREE,
-        ]),
+      disabled: !hasPermission(user, [
+        POLICIES.SUPER_PERMISSION,
+        POLICIES.LIST_TREE,
+        POLICIES.APPROVE_TREE,
+      ]),
     },
     {
       name: 'Trees',
       linkTo: '/trees',
       component: Trees,
       icon: IconNature,
-      disabled: !hasPermission(
-        user, [
-          POLICIES.SUPER_PERMISSION,
-          POLICIES.LIST_TREE,
-        ]),
+      disabled: !hasPermission(user, [
+        POLICIES.SUPER_PERMISSION,
+        POLICIES.LIST_TREE,
+      ]),
     },
     {
       name: 'Planters',
       linkTo: '/planters',
       component: Planters,
       icon: IconGroup,
-      disabled: !hasPermission(
-        user, [
-          POLICIES.SUPER_PERMISSION,
-          POLICIES.LIST_PLANTER,
-        ]),
+      disabled: !hasPermission(user, [
+        POLICIES.SUPER_PERMISSION,
+        POLICIES.LIST_PLANTER,
+      ]),
     },
     {
       name: 'Payments',
@@ -88,10 +85,9 @@ function getRoutes(user) {
       icon: CategoryIcon,
       //TODO this is temporarily, need to add species policy
       disabled:
-        (!hasPermission(user, [
-          POLICIES.SUPER_PERMISSION,
-          POLICIES.LIST_TREE,
-        ])) || !user || user.policy.organization !== undefined,
+        !hasPermission(user, [POLICIES.SUPER_PERMISSION, POLICIES.LIST_TREE]) ||
+        !user ||
+        user.policy.organization !== undefined,
     },
     {
       name: 'Settings',
@@ -105,10 +101,7 @@ function getRoutes(user) {
       linkTo: '/usermanager',
       component: Users,
       icon: IconGroup,
-      disabled: !hasPermission(
-        user, [
-          POLICIES.SUPER_PERMISSION,
-        ]),
+      disabled: !hasPermission(user, [POLICIES.SUPER_PERMISSION]),
     },
     {
       name: 'Account',
@@ -117,88 +110,91 @@ function getRoutes(user) {
       icon: IconPermIdentity,
       disabled: false,
     },
-  ]
+  ];
 }
 
 export const AppProvider = (props) => {
-  const localUser = JSON.parse(localStorage.getItem('user'))
-  const [user, setUser] = React.useState(undefined)
-  const [token, setToken] = React.useState(undefined)
+  const localUser = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = React.useState(undefined);
+  const [token, setToken] = React.useState(undefined);
   const [routes, setRoutes] = React.useState(getRoutes(localUser));
 
   function checkSession() {
-    const localToken = JSON.parse(localStorage.getItem('token'))
-    const localUser = JSON.parse(localStorage.getItem('user'))
+    const localToken = JSON.parse(localStorage.getItem('token'));
+    const localUser = JSON.parse(localStorage.getItem('user'));
     if (localToken && localUser) {
       // Temporarily log in with the localStorage credentials while
       // we check that the session is still valid
-      context.login(localUser, localToken)
+      context.login(localUser, localToken);
 
-      axios.get(
-        `${process.env.REACT_APP_API_ROOT}/auth/check_session?id=${localUser.id}`,
-        {
-          headers: { Authorization: localToken },
-        }
-      ).then((response) => {
-        console.log('CONTEXT CHECK SESSION', response, 'localUser', localUser, 'localToken', localToken);
-        if (response.status === 200) {
-          if (response.data.token === undefined) {
-            //the role has not changed
-            context.login(localUser, localToken, true)
+      axios
+        .get(
+          `${process.env.REACT_APP_API_ROOT}/auth/check_session?id=${localUser.id}`,
+          {
+            headers: { Authorization: localToken },
+          },
+        )
+        .then((response) => {
+          // console.log('CONTEXT CHECK SESSION', response.data, 'USER', localUser, 'TOKEN', localToken);
+          if (response.status === 200) {
+            if (response.data.token === undefined) {
+              //the role has not changed
+              context.login(localUser, localToken, true);
+            } else {
+              //role has changed, update the token
+              context.login(localUser, response.data.token, true);
+            }
           } else {
-            //role has changed, update the token
-            context.login(localUser, response.data.token, true)
+            context.logout();
           }
-        } else if (response.status === 401) {
-          // Unauthorized - log out
-          context.logout()
-        }
-      })
-      return true
+        });
+      return true;
     }
-    return false
+    return false;
   }
 
   const context = {
     login: (newUser, newToken, rememberDetails) => {
       // This api gets hit with identical users from multiple login calls
       if (!isEqual(session.user, newUser)) {
-        setUser(newUser)
-        session.user = newUser
+        setUser(newUser);
+        session.user = newUser;
         if (rememberDetails) {
-          localStorage.setItem('user', JSON.stringify(newUser))
+          localStorage.setItem('user', JSON.stringify(newUser));
         }
 
         // By not updating routes object, we can memoize the menu and routes better
-        setRoutes(getRoutes(newUser))
+        setRoutes(getRoutes(newUser));
       }
 
       if (session.token !== newToken) {
-        setToken(newToken)
-        session.token = newToken
+        setToken(newToken);
+        session.token = newToken;
 
         if (rememberDetails) {
-          localStorage.setItem('token', JSON.stringify(newToken))
+          localStorage.setItem('token', JSON.stringify(newToken));
         }
       }
     },
     logout: () => {
-      setUser(undefined)
-      setToken(undefined)
-      setRoutes(getRoutes(undefined))
-      session.token = undefined
-      session.user = undefined
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
+      setUser(undefined);
+      setToken(undefined);
+      setRoutes(getRoutes(undefined));
+      session.token = undefined;
+      session.user = undefined;
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
     },
     user,
     token,
     routes,
-  }
+  };
 
   if (!user || !token) {
-    checkSession()
+    checkSession();
   }
 
-  return <AppContext.Provider value={context}>{props.children}</AppContext.Provider>
-}
+  return (
+    <AppContext.Provider value={context}>{props.children}</AppContext.Provider>
+  );
+};
