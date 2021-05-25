d31015140261b4c33339e28aca5c05013e5397c0
Carlos Martins <luacmartins@gmail.com>
2021-05-25 09:33:53 -0600

    feat: stakeholder initial commit

---
+++ src/components/Stakeholders/FilterBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import Filter from './Filter';
+import Add from './Add';
+
+const useStyles = makeStyles({
+  flex: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  spaceBetween: {
+    justifyContent: 'space-between',
+  },
+  my: {
+    marginTop: '3rem',
+    marginBottom: '2rem',
+  },
+});
+
+export default function FilterBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={`${classes.flex} ${classes.spaceBetween} ${classes.my}`}>
+      <div>
+        <Filter />
+      </div>
+      <div className={classes.flex}>
+        <Add />
+      </div>
+    </div>
+  );
+}
