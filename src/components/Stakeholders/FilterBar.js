65b7d4b18eecfb67f973c2da2d68943c9111e7d0
Carlos Martins <luacmartins@gmail.com>
2021-05-31 20:21:07 -0600

    fix: remove unused imports

--- src/components/Stakeholders/FilterBar.js
+++ src/components/Stakeholders/FilterBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
 import Filter from './Filter';
 import Add from './Add';
 
