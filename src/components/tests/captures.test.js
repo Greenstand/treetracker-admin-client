91ddbd3087239a4559ecd0d22b32ae9230a3b3fa
tranquanghuy0801 <huytran.quang080199@gmail.com>
2021-09-22 19:45:37 +1000

    feat: refactoring renaming planters to growers

--- src/components/tests/captures.test.js
+++ src/components/tests/captures.test.js
@@ -159,7 +159,7 @@ describe.skip('Captures', () => {
         expect(item).toBeInTheDocument();
         item = within(table).getByText(/Capture ID/i);
         expect(item).toBeInTheDocument();
-        item = within(table).getByText(/Planter ID/i);
+        item = within(table).getByText(/Grower ID/i);
         expect(item).toBeInTheDocument();
         item = within(table).getByText(/Payment/i);
         expect(item).toBeInTheDocument();
