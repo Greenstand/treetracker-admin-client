78551f35e1c771e8ff173518703ae267808dec22
Nick Charlton <nmcharlton@hotmail.com>
2021-09-24 21:44:34 +0100

    doc: fix typo in comment

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,7 +1,7 @@
 /**
  * @function
  * @name stringToSearchRegExp
- * @description Converts a string to a case-insesnsitive regular expression
+ * @description Converts a string to a case-insensitive regular expression
  * that can be used to search for string patterns.
  *
  * @param {string} value The string to convert.
