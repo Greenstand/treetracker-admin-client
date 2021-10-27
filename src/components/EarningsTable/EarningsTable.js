be9d172a9245deec5757c8b24d7e8da4de71ef9f
mloweedgar <mloweedgar0@gmail.com>
2021-10-27 20:58:11 +0300

    feat(earnings table): style side menu with css

--- src/components/EarningsTable/EarningsTable.js
+++ src/components/EarningsTable/EarningsTable.js
@@ -1,6 +1,45 @@
 import Grid from '@material-ui/core/Grid';
-import Navbar from '../Navbar';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Menu, { MENU_WIDTH } from '../common/Menu';
+
+/**
+ * @function
+ * @name styles
+ * @description styles for EarningsTable component
+ *
+ * @returns {Object} css classes
+ */
+const style = () => ({
+  box: {
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+  },
+  menuAside: {
+    height: '100%',
+    width: MENU_WIDTH,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+  menu: {
+    height: '100%',
+    width: MENU_WIDTH,
+    overflow: 'hidden',
+  },
+  rightBox: {
+    height: '100%',
+    position: 'absolute',
+    padding: '40px',
+    left: MENU_WIDTH,
+    top: 0,
+    right: 0,
+    backgroundColor: 'rgb(239, 239, 239)',
+    boxSizing: 'border-box',
+  },
+});
 
 /**
  * @function
@@ -8,20 +47,26 @@ import React from 'react';
  * @description displays table containing  earnings data
  * @param {object} props - component props
  * @param {object} props.earnings - earnings data
+ * @param {object} props.classes - css classes
  *
  * @returns {React.Component} earnings table
  */
-function EarningsTable() {
+function EarningsTable(props) {
+  const { classes } = props;
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Navbar />
-      </Grid>
-      <Grid item>
-        <h2>EarningsTable Works</h2>
-      </Grid>
-    </Grid>
+    <div className={classes.box}>
+      <div className={classes.menuAside}>
+        <Paper elevation={3} className={classes.menu}>
+          <Menu variant="plain" />
+        </Paper>
+      </div>
+      <div className={classes.rightBox}>
+        <Grid item>
+          <h2>EarningsTable Works</h2>
+        </Grid>
+      </div>
+    </div>
   );
 }
 
-export default EarningsTable;
+export default withStyles(style)(EarningsTable);
