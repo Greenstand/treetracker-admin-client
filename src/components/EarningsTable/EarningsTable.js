f7aa91003f1fde19abf6cd5081ebc496304da44e
mloweedgar <mloweedgar0@gmail.com>
2021-10-28 10:40:42 +0300

    feat(earnings top bar): add import and export buttons

--- src/components/EarningsTable/EarningsTable.js
+++ src/components/EarningsTable/EarningsTable.js
@@ -1,9 +1,85 @@
-import Grid from '@material-ui/core/Grid';
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import styles from './EarningsTable.styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import PublishIcon from '@material-ui/icons/Publish';
+import useStyles from './EarningsTable.styles';
 import Menu from '../common/Menu';
+
+/**
+ * @function
+ * @name EarningsLeftMenu
+ * @description renders left menu
+ *
+ * @param {Object} props - component props
+ * @param {Object} props.classes - component css classes
+ * @returns {React.Component} left menu
+ */
+function EarningsLeftMenu(props) {
+  const classes = useStyles();
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3} className={classes.menu}>
+        <Menu variant="plain" />
+      </Paper>
+    </Grid>
+  );
+}
+
+/**
+ * @function
+ * @name EarningsTableTopar
+ * @description renders earnings table top bar
+ *
+ * @param {Object} props - component props
+ * @returns
+ */
+function EarningsTableTopar(props) {
+  const classes = useStyles();
+  return (
+    <Grid item xs={9} container className={classes.rightBox}>
+      <Grid item xs={12}>
+        <Grid container justify="space-between" alignItems="flex-end">
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Typography variant="h2">Earnings</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item className={classes.earningsTableTopBarRight}>
+            <Grid container justify="space-around">
+              <Grid item>
+                <Grid
+                  container
+                  direction="row"
+                  alignItems="center"
+                  className={classes.dowloadButton}
+                >
+                  <GetAppIcon />
+                  <Typography variant="h5">EXPORT</Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid
+                  container
+                  direction="row"
+                  alignItems="center"
+                  className={classes.dowloadButton}
+                >
+                  <PublishIcon />
+                  <Typography variant="h5">IMPORT</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
 
 /**
  * @function
@@ -16,21 +92,13 @@ import Menu from '../common/Menu';
  * @returns {React.Component} earnings table
  */
 function EarningsTable(props) {
-  const { classes } = props;
+  const classes = useStyles();
   return (
-    <div className={classes.box}>
-      <div className={classes.menuAside}>
-        <Paper elevation={3} className={classes.menu}>
-          <Menu variant="plain" />
-        </Paper>
-      </div>
-      <div className={classes.rightBox}>
-        <Grid item>
-          <h2>EarningsTable Works</h2>
-        </Grid>
-      </div>
-    </div>
+    <Grid container className={classes.box}>
+      <EarningsLeftMenu />
+      <EarningsTableTopar />
+    </Grid>
   );
 }
 
-export default withStyles(styles)(EarningsTable);
+export default EarningsTable;
--- src/components/EarningsTable/EarningsTable.styles.js
+++ src/components/EarningsTable/EarningsTable.styles.js
@@ -1,13 +1,32 @@
 import { MENU_WIDTH } from '../common/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
 /**
- * @function
- * @name styles
- * @description styles for EarningsTable component
- *
- * @returns {Object} css classes
+ * @object
+ * @name EarningsTableTopBarStyles
+ * @description styles for EarningsTableTopBar component
  */
-const styles = () => ({
+const earningsTableTopBarStyles = {
+  earningsTableTopBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  dowloadButton: {
+    color: '#86C232',
+  },
+
+  earningsTableTopBarRight: {
+    width: '60%',
+    height: '100%',
+  },
+};
+
+/**
+ * @object
+ * @name EarningsTableStyles
+ * @description styles for EarningsTable component
+ */
+const earningsTableStyles = {
   box: {
     width: '100%',
     height: '100%',
@@ -35,6 +54,17 @@ const styles = () => ({
     backgroundColor: 'rgb(239, 239, 239)',
     boxSizing: 'border-box',
   },
-});
+};
 
-export default styles;
+/**
+ * @function
+ * @name useStyles
+ * @description hook that combines all the styles
+ * @returns {object} styles
+ */
+const useStyles = makeStyles(() => ({
+  ...earningsTableStyles,
+  ...earningsTableTopBarStyles,
+}));
+
+export default useStyles;
