ad1e73a5bd15bc007c68676e8c8c31f27b36b00e
mloweedgar <mloweedgar0@gmail.com>
2021-11-08 22:42:04 +0300

    refactor(earnings table): put table header and body into their own components

--- src/components/EarningsTable/EarningsTable.js
+++ src/components/EarningsTable/EarningsTable.js
@@ -6,9 +6,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableBody from '@material-ui/core/TableBody';
 import Grid from '@material-ui/core/Grid';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import GetAppIcon from '@material-ui/icons/GetApp';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import TablePagination from '@material-ui/core/TablePagination';
 import Typography from '@material-ui/core/Typography';
 import API from '../../api/treeTrackerApi';
@@ -16,18 +13,68 @@ import useStyles from './EarningsTable.styles';
 
 /**
  * @function
+ * @name EarningsTableTopar
+ * @description renders earnings table top bar which contains table actions(i.e. filter, export, etc)
+ *
+ * @returns
+ */
+function EarningsTableTopar() {
+  const classes = useStyles();
+  return (
+    <Grid container direction="row" alignItems="center" justify="space-between">
+      <Grid item className={classes.earningsTableTopBarTitle}>
+        <Typography variant="h4">Earnings</Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+/**
+ * @function
+ * @name EarningsTableHead
+ * @description renders earnings table head columns dynamically
+ * @param {object} props
+ * @param {string} props.columns
+ *
+ * @returns {React.Component} earnings table head columns
+ */
+const EarningsTableHead = ({ columns }) => {
+  const classes = useStyles();
+
+  return (
+    <TableHead>
+      <TableRow className={classes.earningsTableHeader}>
+        {columns.map((column, i) => (
+          <TableCell key={`${i}-${column}`}>
+            <Typography variant="h6">
+              {column}
+              {i === columns.length - 1 && (
+                <InfoOutlinedIcon className={classes.infoIcon} />
+              )}
+            </Typography>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+/**
+ * @function
  * @name EarningsTablePagination
  * @description renders table pagination
  *
+ * @param {object} props
+ * @param {string} props.total - total earnings
+ *
  * @returns {React.Component} earnings table pagination
  */
-const EarningsTablePagination = (props) => {
+const EarningsTablePagination = ({ total }) => {
   const classes = useStyles();
-  const { totalCount } = props;
 
   return (
     <TablePagination
-      count={totalCount}
+      count={total}
       classes={{
         selectRoot: classes.selectRoot,
         root: classes.earningsTablePagination,
@@ -48,33 +95,33 @@ const EarningsTablePagination = (props) => {
 
 /**
  * @function
- * @name EarningsTableTopar
- * @description renders earnings table top bar
+ * @name EarningsTableBody
+ * @description renders earnings table body rows dynamically
+ * @param {object} props
+ * @param {object} props.data
+ * @param {string} props.columns
+ * @param {string} props.total
  *
- * @returns
+ * @returns {React.Component} earnings table body rows
  */
-function EarningsTableTopar() {
-  const classes = useStyles();
+const EarningsTableBody = ({ data, columns, total }) => {
   return (
-    <Grid container direction="row" alignItems="center" justify="space-between">
-      <Grid item className={classes.earningsTableTopBarTitle}>
-        <Typography variant="h4">Earnings</Typography>
-      </Grid>
-    </Grid>
+    <TableBody>
+      {data.map((row, i) => (
+        <TableRow key={`${i}-${row.id}`}>
+          {columns.map((column, j) => (
+            <TableCell key={`${i}-${j}-${column}`}>
+              <Typography variant="body1">{row[column]}</Typography>
+            </TableCell>
+          ))}
+        </TableRow>
+      ))}
+      <TableRow>
+        <EarningsTablePagination totalCount={total} />
+      </TableRow>
+    </TableBody>
   );
-}
-
-/**
- * @Array
- * @name earnings table header columns
- */
-const headerColumns = [
-  'Grower',
-  'Funder',
-  'Amount',
-  'Payment System',
-  'Effective Date',
-];
+};
 
 /**
  * @function
@@ -82,82 +129,36 @@ const headerColumns = [
  * @description displays table containing  earnings data
  * @returns {React.Component} earnings table
  */
-function EarningsTable() {
+export default function EarningsTable() {
   const classes = useStyles();
   const [earnings, setEarnings] = useState([]);
   const [totalCount, setTotalCount] = useState(0);
+
+  const headerColumns = [
+    'Grower',
+    'Funder',
+    'Amount',
+    'Payment System',
+    'Effective Date',
+  ];
+
+  const bodyColumns = [
+    'grower',
+    'funder',
+    'amount',
+    'paymentSystem',
+    'effectiveDate',
+  ];
 
   async function fetchEarnings() {
     const response = await API.getEarnings();
     setEarnings(response.earnings);
     setTotalCount(response.totalCount);
-    console.log('earnings loaded---------------', response);
   }
 
   useEffect(() => {
     fetchEarnings();
   }, []);
-
-  /**
-   * @function
-   * @name renderTableHeaderColumns
-   * @description renders table header columns
-   *
-   * @param {Array} columns - table header columns
-   * @returns {JSX} table header columns
-   */
-  const renderTableHeaderColumns = (columns) => (
-    <TableRow className={classes.earningsTableHeader}>
-      {columns.map((column, i) => (
-        <TableCell key={`${i}-${column}`}>
-          <Typography variant="h6">
-            {column}
-            {i === columns.length - 1 && (
-              <InfoOutlinedIcon className={classes.infoIcon} />
-            )}
-          </Typography>
-        </TableCell>
-      ))}
-    </TableRow>
-  );
-
-  /**
-   * @function
-   * @name renderTableBodyRows
-   * @description renders table body rows
-   * @param {Array} earnings - earnings to render
-   * @returns {JSX} table body rows
-   */
-  const renderTableBodyRows = (earnings) =>
-    earnings.map((earning, i) => (
-      <TableRow key={earning.id}>
-        <TableCell
-          classes={i === earnings.length - 1 ? { root: classes.root } : {}}
-        >
-          {earning.name}
-        </TableCell>
-        <TableCell
-          classes={i === earnings.length - 1 ? { root: classes.root } : {}}
-        >
-          {earning.funder}
-        </TableCell>
-        <TableCell
-          classes={i === earnings.length - 1 ? { root: classes.root } : {}}
-        >
-          {earning.amount}
-        </TableCell>
-        <TableCell
-          classes={i === earnings.length - 1 ? { root: classes.root } : {}}
-        >
-          {earning.paymentSystem}
-        </TableCell>
-        <TableCell
-          classes={i === earnings.length - 1 ? { root: classes.root } : {}}
-        >
-          {earning.effectivePaymentDate}
-        </TableCell>
-      </TableRow>
-    ));
 
   return (
     <Grid container direction="column" className={classes.earningsTable}>
@@ -166,17 +167,14 @@ function EarningsTable() {
       </Grid>
       <Grid item>
         <Table>
-          <TableHead>{renderTableHeaderColumns(headerColumns)}</TableHead>
-          <TableBody>
-            {renderTableBodyRows(earnings)}
-            <TableRow>
-              <EarningsTablePagination totalCount={totalCount} />
-            </TableRow>
-          </TableBody>
+          <EarningsTableHead columns={headerColumns} />
+          <EarningsTableBody
+            data={earnings}
+            columns={bodyColumns}
+            total={totalCount}
+          />
         </Table>
       </Grid>
     </Grid>
   );
 }
-
-export default EarningsTable;
