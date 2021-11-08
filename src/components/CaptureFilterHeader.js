5fb79219a4733e2c1ed55337dea0c55a5355c16a
Gwynn DP <gwynndp@gmail.com>
2021-11-08 12:46:38 -0800

    refactor: add filter counter to header

--- src/components/CaptureFilterHeader.js
+++ src/components/CaptureFilterHeader.js
@@ -1,13 +1,28 @@
 import React, { useState, useContext } from 'react';
 import { Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import IconFilter from '@material-ui/icons/FilterList';
 import { CapturesContext } from '../context/CapturesContext';
 import Navbar from './Navbar';
 import CaptureFilter from './CaptureFilter';
 
-function CaptureFilterHeader() {
+const useStyle = makeStyles((theme) => ({
+  activeFilters: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+    marginLeft: '0.75rem',
+    backgroundColor: theme.palette.stats.green,
+    fontSize: 'smaller',
+    fontWeight: 'bold',
+  },
+}));
+
+function CaptureFilterHeader(props) {
+  const classes = useStyle(props);
   const capturesContext = useContext(CapturesContext);
   const [isFilterShown, setFilterShown] = useState(true);
+  const numFilters = capturesContext.filter.countAppliedFilters();
 
   const handleFilterSubmit = (filter) => {
     capturesContext.updateFilter(filter);
@@ -22,13 +37,16 @@ function CaptureFilterHeader() {
       <Navbar
         buttons={[
           <Button
-            variant="text"
+            variant="outlined"
             color="primary"
             onClick={handleFilterClick}
             startIcon={<IconFilter />}
             key={1}
           >
             Filter
+            {numFilters > 0 && (
+              <Avatar className={classes.activeFilters}>{numFilters}</Avatar>
+            )}
           </Button>,
         ]}
       >
