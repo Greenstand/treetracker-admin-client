37a75c3fdcf5c0749135860d4713db70ddb42894
mloweedgar <mloweedgar0@gmail.com>
2021-12-02 08:35:38 +0300

    feat(api): add earnings api client

---
+++ src/api/earnings.js
@@ -0,0 +1,25 @@
+import { handleResponse, handleError } from './apiUtils';
+import { session } from '../models/auth';
+
+const apiUrl = `${process.env.REACT_APP_API_URL}/${process.env.REACT_APP_EARNINGS_API_MAPPING}`;
+
+export default {
+  /**
+   * @function getEarnings
+   * @description Get earnings from the API
+   * @returns {Promise}
+   */
+  async getEarnings() {
+    const localUrl = `earnings.json`;
+    const endpoint = `${apiUrl}earnings`;
+    return fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: session.token,
+      },
+    })
+      .then(handleResponse)
+      .catch(handleError);
+  },
+};
