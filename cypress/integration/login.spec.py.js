f11674bf2eddaed78f0921d61d65dc388234f566
PtrHtn <ptrhtn@gmail.com>
2022-01-10 01:35:34 -0300

    test: add "button LOG IN" cypress integration test
    until one of the fields, user name or password, is empty, the button LOG IN will remain grayed out

--- cypress/integration/login.spec.py.js
+++ cypress/integration/login.spec.py.js
@@ -20,6 +20,20 @@ describe.only('Login', () => {
     cy.get('input[type="checkbox"]').should('be.checked');
     cy.get('input[type="checkbox"]').click().should('not.be.checked');
   });
+
+  it('until one of the fields, user name or password, is empty, the button LOG IN will remain grayed out', () => {
+    cy.visit('/');
+
+    cy.get('input[id="userName"]').type('John');
+    cy.get('input[id="userName"]').get('value').should('not.be.empty');
+    cy.get('input[id="password"]').should('be.empty');
+    cy.get('button[type="submit"]').should('be.disabled');
+
+    cy.get('input[id="userName"]').clear().should('be.empty');
+    cy.get('input[id="password"]').type('Secret Password');
+    cy.get('input[id="password"]').get('value').should('not.be.empty');
+    cy.get('button[type="submit"]').should('be.disabled');
+  });
 });
 
 describe('Login', () => {
