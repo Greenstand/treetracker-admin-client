e56d50f3f8b15651792805c9c84423c43417c98c
mloweedgar <mloweedgar0@gmail.com>
2022-01-31 00:52:35 +0300

    feat(common components): implement re-usable filter

---
+++ src/components/common/CustomTableFilter/CustomTableFilter.styles.js
@@ -0,0 +1,63 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+/**
+ * @function
+ * @name customTableFilterStyles
+ * @description styles for CustomTableFilter component
+ * @param {object} theme - material-ui theme object
+ * @returns {object} styles object
+ */
+const customTableFilterStyles = (theme) => ({
+  customTableFilterHeader: {
+    padding: theme.spacing(0, 0, 5, 0),
+  },
+  customTableFilterSubmitButton: {
+    marginBottom: theme.spacing(2.5),
+    color: theme.palette.stats.white,
+  },
+  customTableFilterResetButton: {
+    border: 'none',
+  },
+  customTableFilterForm: {
+    width: theme.spacing(80),
+    padding: theme.spacing(5, 4),
+  },
+  customTableFilterCloseIcon: {
+    color: theme.palette.primary.main,
+    cursor: 'pointer',
+    backgroundColor: theme.palette.primary.lightVery,
+  },
+  customTableFilterAvatar: {
+    backgroundColor: theme.palette.primary.lightVery,
+    color: theme.palette.primary.main,
+    marginLeft: theme.spacing(2),
+    width: theme.spacing(7.5),
+    height: theme.spacing(7.5),
+  },
+  customTableFilterSelectFormControl: {
+    margin: theme.spacing(2, 0, 2, 0),
+    width: '100%',
+  },
+
+  // styles for export button
+  csvLink: {
+    color: theme.palette.primary.main,
+    display: 'flex',
+    alignItems: 'flex-end',
+    textDecoration: 'none',
+  },
+});
+
+/**
+ * @function
+ * @name useStyles
+ * @description hook that combines all the styles
+ * @returns {object} styles
+ */
+const useStyles = makeStyles((theme) => {
+  const customTableFilter = customTableFilterStyles(theme);
+
+  return { ...customTableFilter };
+});
+
+export default useStyles;
