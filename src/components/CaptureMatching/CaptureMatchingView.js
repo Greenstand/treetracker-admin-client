0c77e6983b666c34ba2cae8f7faa6c1dc0175923
deanchen <antlet2004@gmail.com>
2022-03-28 14:25:15 +0800

    fix: show message inporperly

--- src/components/CaptureMatching/CaptureMatchingView.js
+++ src/components/CaptureMatching/CaptureMatchingView.js
@@ -635,14 +635,17 @@ function CaptureMatchingView(props) {
                 sameTreeHandler={sameTreeHandler}
               />
             )}
-            {!loading && captureImage && candidateImgData && (
-              //captureImage && treesCount === 0 && (
-              <Box className={classes.noCandidateBox}>
-                <Typography variant="h5">
-                  No candidate match found, this capture might be a new tree
-                </Typography>
-              </Box>
-            )}
+            {!loading &&
+              captureImage &&
+              candidateImgData &&
+              candidateImgData.length === 0 && (
+                //captureImage && treesCount === 0 && (
+                <Box className={classes.noCandidateBox}>
+                  <Typography variant="h5">
+                    No candidate match found, this capture might be a new tree
+                  </Typography>
+                </Box>
+              )}
           </Box>
         </Box>
         {loading && (
