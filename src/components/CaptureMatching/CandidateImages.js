172ce0957e69ea4ae393657102d12cec6a40fd3b
Alkin Maystorov <76817540+SirDev97@users.noreply.github.com>
2022-04-10 18:51:18 +0200

    fix: matching trees' layout horizontal scrolling bug (#636)
    * fix: add solution for horizontal scrolling issue
    
    * chore: remove unnecessary display import

--- src/components/CaptureMatching/CandidateImages.js
+++ src/components/CaptureMatching/CandidateImages.js
@@ -50,7 +50,7 @@ const useStyles = makeStyles({
     flexDirection: 'row',
     overflowX: 'auto',
     overflowY: 'hidden',
-    gap: theme.spacing(2),
+    gap: theme.spacing(4),
   },
 
   candidateImgBtn: {
@@ -84,7 +84,7 @@ const useStyles = makeStyles({
     textAlign: 'center',
   },
   candidateCaptureContainer: {
-    width: '253px',
+    width: '300px',
     height: '340px',
     position: 'relative',
   },
@@ -228,6 +228,10 @@ function CandidateImages({ capture, candidateImgData, sameTreeHandler }) {
                             src={candidateCapture.image_url}
                             alt={`Candidate capture ${candidateCapture.id}`}
                             objectFit="cover"
+                            style={{
+                              width: '300px',
+                              height: '100%',
+                            }}
                           />
                           <Box className={classes.captureInfo}>
                             <Box className={classes.captureInfoDetail}>
