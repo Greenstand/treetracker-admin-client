6f1b0a5c730d03857bbeee875f56d2337326966c
Gwynn Dandridge-Perry <gwynndp@gmail.com>
2022-05-29 19:29:44 -0700

    feat: update TagsContext to reference tags from treetracker api

--- src/context/TagsContext.js
+++ src/context/TagsContext.js
@@ -24,7 +24,7 @@ export function TagsProvider(props) {
 
   // EVENT HANDLERS
   const loadTags = async () => {
-    const tags = await api.getTags();
+    const { tags } = await api.getTags();
     log.debug('load tags from api:', tags.length);
     setTagList(tags);
   };
