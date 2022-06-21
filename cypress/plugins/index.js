83a53561771dff638aa91736dea3170d2dcee73a
Krithin Jay Pakshootra <krithinpakshootra@Devs-MacBook-Air.local>
2022-06-21 19:45:21 +0800

    fix: cypress 9.5.0 bug

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,13 @@
 // cypress/plugins/index.js
 module.exports = (on, config) => {
-  require('cypress-react-unit-test/plugins/react-scripts')(on, config)
-  // IMPORTANT to return the config object
-  // with the any changed environment variables
-  return config
-}
+  if (config.testingType === 'component') {
+    const { startDevServer } = require('@cypress/webpack-dev-server');
+
+    // Your project's Webpack configuration
+    const webpackConfig = require('../../webpack.config.js');
+
+    on('dev-server:start', (options) =>
+      startDevServer({ options, webpackConfig })
+    );
+  }
+};
