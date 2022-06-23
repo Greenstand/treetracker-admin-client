de2bbcb57d1f7c69477f6f9cf6068d1c1540f34e
Gwynn Dandridge-Perry <gwynndp@gmail.com>
2022-06-23 15:35:37 -0700

    fix: prevent requesting the capture detail again

--- src/context/CaptureDetailContext.js
+++ src/context/CaptureDetailContext.js
@@ -37,15 +37,17 @@ export function CaptureDetailProvider(props) {
   // EVENT HANDLERS
 
   const getCaptureDetail = async (url, id) => {
-    if (id == null) {
-      log.debug('getCapture called with no id');
-      return Promise.resolve(STATE_EMPTY.capture);
-    }
+    // if (id == null) {
+    //   log.debug('getCapture called with no id');
+    //   return Promise.resolve(STATE_EMPTY.capture);
+    // }
 
-    return api.getCaptureById(url, id).then((capture) => {
-      setState({ ...state, capture });
-      return capture;
-    });
+    if (id) {
+      return api.getCaptureById(url, id).then((capture) => {
+        setState({ ...state, capture });
+        return capture;
+      });
+    }
   };
 
   const getSpecies = async (speciesId) => {
