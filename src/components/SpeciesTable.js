9e0528c5d5c86c1f5204c89ccc06c7f702ec8091
Marwan osama <64364953+marwan-osama@users.noreply.github.com>
2022-08-27 10:04:06 +0200

    fix(species): prevent operations from wrapping (#790)

--- src/components/SpeciesTable.js
+++ src/components/SpeciesTable.js
@@ -116,6 +116,9 @@ const styles = (theme) => ({
   minWidth: {
     minWidth: '320px',
   },
+  operationButton: {
+    padding: '6px',
+  },
 });
 
 const SpeciesTable = (props) => {
@@ -214,10 +217,18 @@ const SpeciesTable = (props) => {
         <TableCell>{species.desc}</TableCell>
         <TableCell>{species.captureCount}</TableCell>
         <TableCell>
-          <IconButton title="edit" onClick={() => handleEdit(species)}>
+          <IconButton
+            title="edit"
+            onClick={() => handleEdit(species)}
+            className={classes.operationButton}
+          >
             <Edit />
           </IconButton>
-          <IconButton title="delete" onClick={() => openDeleteDialog(species)}>
+          <IconButton
+            title="delete"
+            onClick={() => openDeleteDialog(species)}
+            className={classes.operationButton}
+          >
             <Delete />
           </IconButton>
         </TableCell>
