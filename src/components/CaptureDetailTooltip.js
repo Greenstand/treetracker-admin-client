0f171234e3b22c98417da8dfb112401c080f155b
Andres Beltran <66681471+andres039@users.noreply.github.com>
2022-08-30 14:35:53 -0400

    Update CaptureDetailTooltip.js

--- src/components/CaptureDetailTooltip.js
+++ src/components/CaptureDetailTooltip.js
@@ -28,6 +28,7 @@ const CaptureDetailTooltip = ({ capture, showCaptureClick }) => {
   const { speciesList } = useContext(SpeciesContext);
 
   React.useEffect(() => {
+  setSpeciesName();
     if (capture?.speciesId) {
       const currentSpecies = () => {
         return speciesList.find((species) => species.id === capture.speciesId);
@@ -37,7 +38,6 @@ const CaptureDetailTooltip = ({ capture, showCaptureClick }) => {
       setSpeciesName(speciesData.name);
       }
     }
-    return () => setSpeciesName();
   }, [capture]);
 
   const CaptureDetailTooltipStyles = CaptureDetailTooltipUseStyles();
