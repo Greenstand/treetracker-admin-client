52b81550608d70edb410d935bc8d94b3b01ca467
dharmiknotop <mukundbhai634@gmail.com>
2022-10-04 21:40:14 +0530

    fix: added a spinner while the image loads up in capture page

--- src/components/Captures/CaptureTooltip.js
+++ src/components/Captures/CaptureTooltip.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OptimizedImage from '../OptimizedImage';
-import { Box } from '@material-ui/core';
+import { Box, CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(() => ({
+  spinner: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+  },
+}));
+
 const CaptureTooltip = ({ capture, toggleDrawer }) => {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+  const classes = useStyles();
   return (
     <Box>
+      {isImageLoading && (
+        <Box className={classes.spinner}>
+          <CircularProgress />
+        </Box>
+      )}
       <OptimizedImage
         onClick={toggleDrawer(capture.id)}
         src={capture.imageUrl}
@@ -14,6 +32,9 @@ const CaptureTooltip = ({ capture, toggleDrawer }) => {
           width: '160px',
           border: '2px solid black',
           borderRadius: '8px',
+        }}
+        onImageReady={() => {
+          setIsImageLoading(false);
         }}
       />
     </Box>
