0c0e3901c647ae3a05c0f5a853393eaee3bf8388
Vlad-Pischaeff <48124219+Vlad-Pischaeff@users.noreply.github.com>
2022-10-05 20:33:53 +0500

    fix: growers - multiply network requests with negative results #880 (#881)

--- src/components/Growers/GrowerTooltip.js
+++ src/components/Growers/GrowerTooltip.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Box, Card, CardActionArea, Divider } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { Box, Divider, Typography } from '@material-ui/core';
 import EmailIcon from '@material-ui/icons/EmailOutlined';
 import PhoneIcon from '@material-ui/icons/PhoneOutlined';
 import OrgIcon from '@material-ui/icons/LanguageOutlined';
@@ -9,115 +7,65 @@ import Person from '@material-ui/icons/Person';
 import GrowerOrganization from '../GrowerOrganization';
 import OptimizedImage from '../OptimizedImage';
 import { useStyle } from './Growers.styles.js';
+const SIZE = 32;
 
 const GrowerTooltip = ({ grower, growerClick }) => {
   const classes = useStyle();
   const matches = grower.imageUrl?.match(/\/\/(.*?)\/(.*)/);
-  const useStyles = makeStyles(() => ({
-    box: {
-      display: 'flex',
-      marginTop: '2px',
-    },
-    label: {
-      marginLeft: '12px',
-    },
-  }));
-
-  const growerToolTipStyles = useStyles();
 
   return (
-    <Box>
-      <Box style={{ width: '160px', display: 'block' }}>
-        <Card
-          style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-          onClick={() => growerClick(grower)}
-        >
-          <CardActionArea>
-            <Box style={{ display: 'flex', margin: '4px' }}>
-              <Box
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  margin: '2px',
-                }}
-              >
-                {matches?.length > 1 ? (
-                  <OptimizedImage
-                    rotation={grower.imageRotation}
-                    src={grower.imageUrl}
-                    style={{
-                      height: '24px',
-                      width: '24px',
-                    }}
-                  />
-                ) : (
-                  <Person
-                    className={classes.person}
-                    style={{
-                      height: '24px',
-                      width: '24px',
-                    }}
-                  />
-                )}
-              </Box>
-              <Box>
-                <Typography className={growerToolTipStyles.label}>
-                  {grower.firstName} {grower.lastName}
-                </Typography>
-              </Box>
+    <Box className={classes.tooltipCard} onClick={() => growerClick(grower)}>
+      <Box className={classes.tooltipCardHeader}>
+        {matches?.length > 1 ? (
+          <OptimizedImage
+            rotation={grower.imageRotation}
+            src={grower.imageUrl}
+            width={SIZE}
+            height={SIZE}
+            style={{
+              height: `${SIZE}px`,
+              width: `${SIZE}px`,
+            }}
+          />
+        ) : (
+          <Person style={{ fill: '#ccc' }} />
+        )}
+        <Typography className={classes.tooltipTitle}>
+          {grower.firstName} {grower.lastName}
+        </Typography>
+      </Box>
+
+      <Divider variant="middle" />
+
+      <Box m={1}>
+        {grower.email && (
+          <Box className={classes.tooltipIcon}>
+            <EmailIcon color="primary" />
+            <Typography className={classes.tooltipLabel}>
+              {grower.email}
+            </Typography>
+          </Box>
+        )}
+        {(grower.organizaton || grower.organizationId) && (
+          <Box className={classes.tooltipIcon}>
+            <OrgIcon color="primary" />
+            <Box className={classes.tooltipLabel}>
+              <GrowerOrganization
+                organizationName={grower.organization}
+                assignedOrganizationId={grower.organizationId}
+                compact={true}
+              />
             </Box>
-            <Divider
-              variant="middle"
-              sx={{
-                marginBottom: '2px',
-                marginTop: '4px',
-              }}
-            />
-            <Box
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-start',
-                margin: '0 4px 0 4px',
-              }}
-            >
-              {grower.email && (
-                <Box className={growerToolTipStyles.box}>
-                  <EmailIcon color="primary" />
-                  <Typography className={growerToolTipStyles.label}>
-                    {grower.email}
-                  </Typography>
-                </Box>
-              )}
-              {(grower.organizaton || grower.organizationId) && (
-                <Box className={growerToolTipStyles.box}>
-                  <OrgIcon color="primary" />
-                  <Box className={growerToolTipStyles.label}>
-                    <GrowerOrganization
-                      organizationName={grower.organization}
-                      assignedOrganizationId={grower.organizationId}
-                      compact={true}
-                    />
-                  </Box>
-                </Box>
-              )}
-              {grower.phone && (
-                <Box className={growerToolTipStyles.box}>
-                  <PhoneIcon color="primary" />
-                  <Typography className={growerToolTipStyles.label}>
-                    {grower.phone}
-                  </Typography>
-                </Box>
-              )}
-            </Box>
-          </CardActionArea>
-        </Card>
+          </Box>
+        )}
+        {grower.phone && (
+          <Box className={classes.tooltipIcon}>
+            <PhoneIcon color="primary" />
+            <Typography className={classes.tooltipLabel}>
+              {grower.phone}
+            </Typography>
+          </Box>
+        )}
       </Box>
     </Box>
   );
