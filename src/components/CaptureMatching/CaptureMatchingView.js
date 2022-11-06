d0c3a6791f8521654c91eb03b565d16595685cf1
Janet Yu <janettyu99@gmail.com>
2022-11-05 20:33:16 -0700

    feat: display notes for capture

--- src/components/CaptureMatching/CaptureMatchingView.js
+++ src/components/CaptureMatching/CaptureMatchingView.js
@@ -640,6 +640,18 @@ function CaptureMatchingView() {
               alertTextSize="1rem"
             />
           </Box>
+
+          <Box
+            className={classes.notesContainerBox}
+            margin={4}
+            display="flex"
+            justifyContent="center"
+          >
+            <Typography variant="h6">Notes:</Typography>
+            <Typography variant="body1" className={classes.notesBody}>
+              {captureImage.note}
+            </Typography>
+          </Box>
         </Paper>
       )}
     </Box>
