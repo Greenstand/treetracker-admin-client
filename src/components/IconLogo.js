d8e5bda3cc6d42ead3b34f07f4790c090ed4bf7e
Marcos Spomberg <marcosspomberg@gmail.com>
2023-02-28 20:33:28 -0500

    feat: import and implement useEffect to fetch logo url

--- src/components/IconLogo.js
+++ src/components/IconLogo.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import logo from './images/logo.svg';
 import { AppContext } from '../context/AppContext';
-import { React, useContext, useState } from 'react';
+import { React, useContext, useState, useEffect } from 'react';
 
 /*
  * Just a logo icon
@@ -10,15 +10,20 @@ import { React, useContext, useState } from 'react';
 export default function IconLogo() {
   const appContext = useContext(AppContext);
   const { user } = appContext;
-  const [logoURL, setLogoURL] = useState();
+  const [logoURL, setLogoURL] = useState('');
 
-  if (user) {
-    const STAKEHOLDER_API = process.env.REACT_APP_STAKEHOLDER_API_ROOT;
-    const orgID = user.policy.organization.id;
-    fetch(`${STAKEHOLDER_API}/stakeholders/${orgID}`)
-      .then((response) => response.json())
-      .then((data) => console.log(data.stakeholders[0].logo_url));
-  }
+  useEffect(() => {
+    if (user) {
+      const STAKEHOLDER_API = process.env.REACT_APP_STAKEHOLDER_API_ROOT;
+      const orgID = user.policy.organization.id;
+      fetch(`${STAKEHOLDER_API}/stakeholders/${orgID}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const orgLogo = data.stakeholders[0].logo_url;
+          orgLogo ? setLogoURL(orgLogo) : setLogoURL(logo);
+        });
+    } else setLogoURL(logo);
+  }, [user]);
 
   return (
     <Link to="/">
