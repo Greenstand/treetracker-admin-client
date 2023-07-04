8c1987af04755317b43f95f0a5535d49cef47d93
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2023-07-03 22:26:32 -0500

    fix: null is replaced by 'no organization' on specific cards

--- src/components/reportingCards/ReportingCard6.js
+++ src/components/reportingCards/ReportingCard6.js
@@ -15,6 +15,25 @@ export default function component(props) {
     rows
   );
 
+  const formatData = (data) => {
+    let formattedData = { ...data };
+    if (data?.moreData) {
+      const moreData = data.moreData.map((item) => ({
+        ...item,
+        name: item.name || 'No organization',
+      }));
+      formattedData = { ...formattedData, moreData };
+    }
+    if (data?.top) {
+      const top = data.top.map((item) => ({
+        ...item,
+        name: item.name || 'No organization',
+      }));
+      formattedData = { ...formattedData, top };
+    }
+    return formattedData;
+  };
+
   return (
     <ReportingCard
       text={{
@@ -23,7 +42,7 @@ export default function component(props) {
       }}
       icon={Icon}
       color="#76bb23"
-      data={data}
+      data={data && formatData(data)}
       disableSeeMore={disableSeeMore}
     />
   );
