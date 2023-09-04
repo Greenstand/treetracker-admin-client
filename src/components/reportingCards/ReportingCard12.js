ca74476372eedea5cc8f01694ff3247113e39b33
dadiorchen <antlet2004@gmail.com>
2023-09-04 09:35:17 +0800

    feat: new reporting cards

---
+++ src/components/reportingCards/ReportingCard12.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReportingCard from './ReportingCard';
+import useLoadData from './ReportingCard.hook';
+import NatureIcon from '@material-ui/icons/Nature';
+
+export default function ReportingCard10(props) {
+  const { startDate, endDate, /*disableSeeMore,*/ rows } = props;
+
+  const data = useLoadData(
+    startDate,
+    endDate,
+    'surviving_catchments',
+    'surviving_catchments',
+    (d) => d.average,
+    rows
+  );
+
+  return (
+    <ReportingCard
+      text={{
+        title: 'Surviving Catchments',
+        text1: 'Average',
+      }}
+      icon={NatureIcon}
+      color="#76bb23"
+      data={data}
+      disableSeeMore={true}
+    />
+  );
+}
