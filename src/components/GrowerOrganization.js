30c3df0554733bc10cdc8652b126a765f7120a85
samwel141 <samwelwilson141@gmail.com>
2023-11-13 12:52:31 +0300

    Created separate component for suborganizations

--- src/components/GrowerOrganization.js
+++ src/components/GrowerOrganization.js
@@ -1,19 +1,9 @@
 import React, { useContext } from 'react';
-
+import { Typography } from '@material-ui/core';
 import { AppContext } from '../context/AppContext.js';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
 import { getOrganizationById } from 'utilities/index.js';
-
-import {
-  Paper,
-  List,
-  ListItem,
-  Collapse,
-  FormControlLabel,
-  Radio,
-  ListItemText,
-} from '@material-ui/core';
+import SubOrgs from './SubOrgs.js';
 
 /**
  * @function
@@ -37,70 +27,11 @@ const GrowerOrganization = (props) => {
   );
 
   const renderGrowerOrganization = () => (
-    <Typography style={{ color: '#C0C0C0', fontStyle: 'italic' }}>
-      {organizationName}
-    </Typography>
+    <Typography variant="h6">{organizationName}</Typography>
   );
   const renderGrowerAssignedOrganization = (assignedOrganization) => {
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-    const subOrganizations = ['SubOrg1', 'SubOrg2', 'SubOrg3'];
-
     return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Paper
-          elevation={0}
-          style={{
-            width: '150px',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              padding: '20px',
-              cursor: 'pointer',
-            }}
-          >
-            <Typography variant="h6">
-              {assignedOrganization.name} ({assignedOrganization.id})
-            </Typography>
-          </div>
-          <Collapse in={expanded} style={{ padding: '10px', marginTop: '0' }}>
-            <div>
-              <List>
-                {subOrganizations.map((subOrg, index) => (
-                  <ListItem key={index}>
-                    <ListItemText primary={subOrg} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Collapse>
-        </Paper>
-        <div style={{ position: 'relative' }}>
-          <FormControlLabel
-            control={<Radio checked={expanded} onClick={handleExpandClick} />}
-            label="Show Sub-Orgs"
-            style={{
-              right: '0',
-              position: 'absolute',
-              top: '20px',
-              left: '-160px',
-              display: 'block',
-            }}
-          />
-        </div>
-      </div>
+      <SubOrgs name={assignedOrganization.name} id={assignedOrganization.id} />
     );
   };
   const orgNamesMatch =
